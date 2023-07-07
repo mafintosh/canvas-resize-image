573b380944470d62150de62d76dc7284447bfa24
Mathias Buus <mathiasbuus@gmail.com>
2023-07-07 18:56:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# canvas-resize-image
+
+Resize an image in the browser
+
+```
+npm install canvas-resize-image
+```
+
+## Usage
+
+``` js
+const resize = require('canvas-resize-image')
+
+await resize('base64image', { width, height, url })
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = async function resize (base64, { width = 112, height = 112, url = base64.startsWith('data:') } = {}) {
+  const canvas = new OffscreenCanvas(width, height)
+  const context = canvas.getContext('2d')
+
+  const image = new Image()
+  image.src = base64.startsWith('data:') ? base64 : 'data:;base64,' + base64
+
+  await new Promise((resolve, reject) => {
+    image.onerror = reject
+    image.onload = resolve
+  })
+
+  const aspect = image.width / image.height
+  let x = 0
+  let y = 0
+  let w = canvas.width
+  let h = canvas.height
+  if (aspect > 1) {
+    x = (canvas.width - canvas.width * aspect) / 2
+    w *= aspect
+  } else if (aspect < 1) {
+    y = (canvas.height - canvas.height / aspect) / 2
+    h /= aspect
+  }
+  context.drawImage(image, x, y, w, h)
+  const blob = await canvas.convertToBlob({ type: 'image/webp' })
+  const reader = new FileReader()
+  const read = new Promise((resolve) => { reader.onloadend = resolve })
+  reader.readAsDataURL(blob)
+  await read
+  return url ? reader.result : reader.result.slice(reader.result.indexOf(',') + 1)
+}
